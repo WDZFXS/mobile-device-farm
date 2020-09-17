23917c629bcf97a55b7f5ce2a7ebf7802634f956
d.voylokov <d.voylokov@csat.ru>
2020-09-17 19:27:45 +0300

    init

---
+++ src/main/java/com/wdzfxs/farm/mobiledevicefarmsvc/MobileDeviceFarmSvcApplication.java
@@ -0,0 +1,13 @@
+package com.wdzfxs.farm.mobiledevicefarmsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDeviceFarmSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileDeviceFarmSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wdzfxs/farm/mobiledevicefarmsvc/MobileDeviceFarmSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdzfxs.farm.mobiledevicefarmsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDeviceFarmSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
