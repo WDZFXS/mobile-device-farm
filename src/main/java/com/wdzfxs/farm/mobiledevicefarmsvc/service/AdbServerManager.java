5ea34d6d90a198fe95f6b07798669903863d03c0
wdzfxs <voylokovdima@mail.ru>
2020-12-25 02:05:40 +0300

    Added a adb server stop when service stops
    Signed-off-by: wdzfxs <voylokovdima@mail.ru>

--- src/main/java/com/wdzfxs/farm/mobiledevicefarmsvc/service/AdbServerManager.java
+++ src/main/java/com/wdzfxs/farm/mobiledevicefarmsvc/service/AdbServerManager.java
@@ -2,10 +2,13 @@ package com.wdzfxs.farm.mobiledevicefarmsvc.service;
 
 import com.wdzfxs.farm.mobiledevicefarmsvc.cmd.ConsoleCommands;
 import com.wdzfxs.farm.mobiledevicefarmsvc.util.AdbNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+@Slf4j
 @Component
 public class AdbServerManager {
 
@@ -19,5 +22,10 @@ public class AdbServerManager {
             e.printStackTrace();
             System.exit(0);
         }
+    }
+
+    @PreDestroy
+    public void stopAdbServer() {
+        cc.exec("adb kill-server");
     }
 }
